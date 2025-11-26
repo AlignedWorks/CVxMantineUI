5856970c73a605f3ae0f9bbcbca401692c9bef41
Jordon Byers <jordonbyers@gmail.com>
2025-11-25 19:26:49 -0500

    Remove Promise.withResolvers code

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,3 @@
-if (!Promise.withResolvers) {
-  Promise.withResolvers = function <T>() {
-    let resolve: (value: T | PromiseLike<T>) => void;
-    let reject: (reason?: any) => void;
-    const promise = new Promise<T>((res, rej) => {
-      resolve = res;
-      reject = rej;
-    });
-    return { promise, resolve: resolve!, reject: reject! };
-  };
-}
-
 import ReactDOM from 'react-dom/client';
 import { AuthProvider } from './AuthContext';
 import { CollaborativeProvider } from './CollaborativeContext';
